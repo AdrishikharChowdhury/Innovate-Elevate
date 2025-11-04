7efbfcbcbb95f512c619ad1077ce53156c5c63bd
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2025-11-04 05:01:04 +0000

    Added Navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='bg-black w-full p-5 flex justify-between items-center' >
+        <div className='bg-black w-full p-5 flex justify-between items-center h-20' >
             <h1>Insert Name Here</h1>
             <ul className='flex justify-center items-center gap-5 px-10'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Problem Statements</li>
-                <li>Registration</li>
-                <li>Behind The Scenes</li>
+                <li className='pb-1/2 hover:border-b-2 cursor-pointer transition-all duration-50'>Home</li>
+                <li className='pb-1/2 hover:border-b-2 cursor-pointer transition-all duration-50'>About</li>
+                <li className='pb-1/2 hover:border-b-2 cursor-pointer transition-all duration-50'>Problem Statements</li>
+                <li className='pb-1/2 hover:border-b-2 cursor-pointer transition-all duration-50'>Registration</li>
+                <li className='pb-1/2 hover:border-b-2 cursor-pointer transition-all duration-50'>Behind The Scenes</li>
             </ul>
         </div>
     )
